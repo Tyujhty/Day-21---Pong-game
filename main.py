dd00a71a5a6bcb0097800ef5ca49413dfbd3ae9f
Tyujhty <fannybaquey@gmail.com>
2024-01-21 19:24:22 +0100

    feat: Initialize the ball class. Add the ball moving.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -12,6 +14,8 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
+ball = Ball()
+
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
@@ -21,9 +25,8 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 
 while game_is_on:
+    time.sleep(0.06)
     screen.update()
-
-
+    ball.move_ball()
 
 screen.exitonclick()
-
--- paddle.py
+++ paddle.py
@@ -14,7 +14,6 @@ class Paddle(Turtle):
         new_y = self.ycor() + 20
         self.goto(self.xcor(), new_y)
 
-
     def go_down(self):
         new_y = self.ycor() - 20
         self.goto(self.xcor(), new_y)
