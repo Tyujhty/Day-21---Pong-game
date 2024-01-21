5f657cce9efde27103eefaa0d123fac96817c2d7
Tyujhty <fannybaquey@gmail.com>
2024-01-21 19:51:43 +0100

    feat: Create the method that manage the collisions with the paddles.

--- ball.py
+++ ball.py
@@ -6,6 +6,7 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
+        self.speed("fastest")
         self.x_move = 10
         self.y_move = 10
 
@@ -16,3 +17,6 @@ class Ball(Turtle):
 
     def bounce_y(self):
         self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
--- main.py
+++ main.py
@@ -32,4 +32,8 @@ while game_is_on:
     if ball.ycor() > 280 or ball.ycor() < - 280:
         ball.bounce_y()
 
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 320
+            or ball.distance(l_paddle) < 50 and ball.xcor() < -320):
+        ball.bounce_x()
+
 screen.exitonclick()
